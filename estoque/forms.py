761645dced13c19cddc3ae96277e6870f4c2c38b
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-01-30 15:40:38 -0300

    adiciona validacoes

--- estoque/forms.py
+++ estoque/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import *
+from django.core.exceptions import ValidationError
 
 class ProdutoForm(forms.Form):
   nome = forms.CharField(max_length=225, label="Nome:")
@@ -9,6 +10,33 @@ class ProdutoForm(forms.Form):
   quantidade_estoque = forms.IntegerField(label="Quantidade:")
   fornecedor = forms.ModelChoiceField(queryset=Fornecedor.objects.all())
   categorias = forms.ModelMultipleChoiceField(queryset=Categoria.objects.all())
+
+  def clean_preco(self):
+    preco = self.cleaned_data.get('preco')
+    if not preco > 0:
+      raise ValidationError("o preço tem que ser maior que 0")
+    return preco
+  
+  def clean_quantidade_estoque(self):
+    quantidade_estoque = self.cleaned_data.get('quantidade_estoque')
+    if not isinstance(quantidade_estoque, int):
+      raise ValidationError("deve ser um valor inteiro")
+    if not quantidade_estoque > 0:
+      raise ValidationError("o valor deve ser maior que 0")
+    return quantidade_estoque
+  
+  def clean_codigo(self):
+    codigo = self.cleaned_data.get('codigo')
+    if not codigo.isalnum():
+      raise ValidationError("O codigo não pode ter caracteres diferentes de numero e/ou letras")
+    return codigo
+  
+  def clean_nome(self):
+    nome = self.cleaned_data.get('nome')
+    if len(nome) < 3:
+      raise ValidationError("O nome do produto deve ter pelo menos 3 caracteres.")
+    return nome
+  
 
 class FornecedorForm(forms.Form):
   nome = forms.CharField(max_length=100)
